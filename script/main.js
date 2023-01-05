74268832ea2e33acacb03166380f71cb407121ca
GDLM1208 <gllerena1208@gmail.com>
2023-01-05 13:40:08 -0500

    Actualizacion 1

--- script/main.js
+++
@@ -1,7 +0,0 @@
-var display = document.querySelector("input");
-console.log(display);
-display.addEventListener("change", a);
-
-function a(){
-    document.getElementById("prueba").innerHTML = "Prueba";
-};
