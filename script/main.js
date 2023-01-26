41efe978861f791e62210c938c61318b96b3358e
GDLM1208 <gllerena1208@gmail.com>
2023-01-26 13:52:10 -0500

    mejora del desplazamiento

--- script/main.js
+++ script/main.js
@@ -7,18 +7,11 @@ function scrollToSection(sectionId) {
     // Obtener la posición de la sección correspondiente
     var section = document.getElementById(sectionId);
     var position = section.offsetTop;
-    var sectionSize;
 
-    switch (sectionId) {
-        case "vision":
-            sectionSize = 125;
-            break;
-        case "horarios":
-            sectionSize = 200;
-            break;
-        default:
-            sectionSize = 0;
-    }
+    var navBar = document.getElementsByClassName("navbar")[0];
+    var sectionSize = getComputedStyle(navBar).height;
+    sectionSize = sectionSize.substring(0, (sectionSize.length - 2));
+
     console.log("position: " + position + "; sectionSize: " + sectionSize);
     window.scrollTo({
         top: position - sectionSize,
